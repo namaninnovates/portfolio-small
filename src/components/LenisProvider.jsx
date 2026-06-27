9f55a88856f9ceb07136385c3b7581dc48c403e4
namaninnovates <naman.innovates@gmail.com>
2026-06-27 22:32:12 +0530

    lenis fix

--- src/components/LenisProvider.jsx
+++ src/components/LenisProvider.jsx
@@ -1,30 +1,11 @@
 "use client";
-import { useEffect } from 'react';
-import Lenis from 'lenis';
+import { ReactLenis } from 'lenis/react';
 import 'lenis/dist/lenis.css';
 
 export default function LenisProvider({ children }) {
-  useEffect(() => {
-    const lenis = new Lenis({
-      smoothWheel: true,
-      syncTouch: false, // Disabling this fixes the weird jumping/scrolling issues on iPhone
-      lerp: 0.08, // The lower the number, the smoother and heavier the inertia feels
-      wheelMultiplier: 1,
-    });
-
-    let rafId;
-    function raf(time) {
-      lenis.raf(time);
-      rafId = requestAnimationFrame(raf);
-    }
-
-    rafId = requestAnimationFrame(raf);
-
-    return () => {
-      lenis.destroy();
-      cancelAnimationFrame(rafId);
-    };
-  }, []);
-
-  return <>{children}</>;
+  return (
+    <ReactLenis root options={{ lerp: 0.08, smoothWheel: true, syncTouch: false }}>
+      {children}
+    </ReactLenis>
+  );
 }
