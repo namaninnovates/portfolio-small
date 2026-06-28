d6dd0011083b7eb9cb55237c7a9a218eac891fff
namaninnovates <naman.innovates@gmail.com>
2026-06-28 23:33:00 +0530

    layout backtick fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -47,13 +47,13 @@ export default function RootLayout({ children }) {
       <head>
         <Script src="https://www.googletagmanager.com/gtag/js?id=G-G69D0FR7FL" strategy="afterInteractive" />
         <Script id="google-analytics" strategy="afterInteractive">
-          {\`
+          {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
             gtag('config', 'G-G69D0FR7FL');
-          \`}
+          `}
         </Script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
